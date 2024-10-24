e9597d44f6bdb1157945f53d591d39493ffe0c1d
azmi fa <azmifauzana55@gmail.com>
2024-10-24 14:05:06 +0700

    First Commit

---
+++ src/Main/Induk.java
@@ -0,0 +1,11 @@
+package Main;
+public class Induk {
+
+public void Cetak(){
+
+    System.out.println("Saya");
+
+}
+
+    
+}
---
+++ src/Main/Pewarisan.java
@@ -0,0 +1,9 @@
+package Main;
+public class Pewarisan {
+    public static void main(String[] args) {
+      Anak test = new Anak();
+      
+      test.Cetak();
+    }
+    
+}
